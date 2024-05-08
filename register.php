f8b70ada55cb36163eec277a02266bcb4520a12d
Aisara1925 <missionjim2002@gmail.com>
2024-05-08 07:17:21 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,59 @@
+<?php 
+    session_start();
+    include('server.php'); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Page</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+        <div class="header">
+            <h2>Register</h2>
+        </div>
+                
+        <form action="register_db.php" method="post">
+            <?php include('errors.php'); ?>
+            <?php if (isset($_SESSION['error'])) : ?>
+            <div class="error">
+                <h3>
+                    <?php
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    ?>
+                </h3>
+            </div>
+        <?php endif ?>
+            <div class="input-group">
+                <lable for="username">Username</lable>
+                <input type="text" name="username">
+            </div>
+            <div class="input-group">
+                <lable for="email">Email</lable>
+                <input type="email" name="email">
+            </div>
+            <div class="input-group">
+                <lable for="password_1">Password</lable>
+                <input type="password" name="password_1">
+            </div>
+            <div class="input-group">
+                <lable for="password_2">Confirm Password</lable>
+                <input type="password" name="password_2">
+            </div>
+            <div class="input-group">
+                <button type="submit" name="ref_user" class="btn">Register</button>
+            </div>
+            <p>Already a member? <a href="login.php">Sign In</a></p>
+        </form>
+
+</body>
+</html>
+
+
+
